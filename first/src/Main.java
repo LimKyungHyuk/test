df0b904d71fe05b9da15321e3f4aeaa672b98e79
khlim <yorsild@naver.com>
2017-05-21 10:36:41 +0900

    1

---
+++ first/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by khlim on 2017. 5. 21..
+ */
+public class Main {
+    public static void main(String[] args) { // psvm
+        System.out.println("Hello world");// sout
+
+    }
+}
