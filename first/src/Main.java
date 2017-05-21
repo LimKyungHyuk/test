5de2a3df67ca73eb57a664371fe5b138d8545aa4
LimKyungHyuk <yorsild@naver.com>
2017-05-21 10:40:35 +0900

    Update Main.java

--- first/src/Main.java
+++ first/src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) { // psvm
-        System.out.println("Hello world");// sout!!
+        System.out.println("Hello world");// sout!!>>
 
     }
 }
